9cc3ec74fe1764a3e49cf9b7dabb7ffca1485c79
Theodorus Clarence <theodorusclarence@gmail.com>
2022-08-18 20:43:19 +0700

    feat: add email whitelist

---
+++ src/lib/email-whitelist.ts
@@ -0,0 +1,22 @@
+const emailWhitelist = [
+  'ppdbultimate@gmail.com',
+  'theodorusclarence@gmail.com',
+  'adhwamaharika@gmail.com',
+  'carlonugroho01@gmail.com',
+  'jeremiakeloko@gmail.com',
+  'ferdinand.parulian@gmail.com',
+  'ivan.19400016@gmail.com',
+  'billharit@gmail.com',
+  'philippurba.pp@gmail.com',
+  'rayhanalifa@googlemail.com',
+  'kanda.wibisanan@gmail.com',
+  'stevearmando@gmail.com',
+  'radityarobert@gmail.com',
+  'sihombingcharminuel@gmail.com',
+  'ryan.ardhana27@gmail.com',
+  'dega.ad@gmail.com',
+  'rayhanalifa@googlemail.com',
+  'williamhadiwijaya111@gmail.com',
+];
+
+export default emailWhitelist;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
+import emailWhitelist from '@/lib/email-whitelist';
 import { prisma } from '@/lib/prisma';
 
 export default NextAuth({
@@ -28,22 +29,3 @@ export default NextAuth({
     },
   },
 });
-
-const emailWhitelist = [
-  'ppdbultimate@gmail.com',
-  'theodorusclarence@gmail.com',
-  'adhwamaharika@gmail.com',
-  'carlonugroho01@gmail.com',
-  'jeremiakeloko@gmail.com',
-  'ferdinand.parulian@gmail.com',
-  'ivan.19400016@gmail.com',
-  'billharit@gmail.com',
-  'philippurba.pp@gmail.com',
-  'rayhanalifa@googlemail.com',
-  'kanda.wibisanan@gmail.com',
-  'stevearmando@gmail.com',
-  'radityarobert@gmail.com',
-  'sihombingcharminuel@gmail.com',
-  'ryan.ardhana27@gmail.com',
-  'dega.ad@gmail.com',
-];
